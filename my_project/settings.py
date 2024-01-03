5e1ad68b900699e90e7dac2de67fa78665e25da5
Saba-Baroughi <saba.baroughi1111@gmail.com>
2024-01-03 12:52:19 +0000

    test

--- my_project/settings.py
+++ my_project/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+## copy from codeinstitute Now we connect the settings.py file to the env.py file:
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^5&runyqw!2yvqf5z*b#g$_t6@u@!f2)h!97$*+0rl92fe2*nj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
@@ -74,10 +79,21 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'xlicohoq',
+        'USER': 'xlicohoq',
+        'PASSWORD': 'lslla0rkDLUg_LP78-A0U5RXS4uqiJds',
+        'HOST': 'mel.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
